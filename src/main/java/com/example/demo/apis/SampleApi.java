d4c0a20d04b0c63567fa387f4e26bac3f49d6542
AD41859 <navaneetha.manickam@lumen.com>
2024-01-10 14:13:58 +0530

    Content changed

--- src/main/java/com/example/demo/apis/SampleApi.java
+++ src/main/java/com/example/demo/apis/SampleApi.java
@@ -11,6 +11,6 @@ public class SampleApi {
     @GetMapping
     public ResponseEntity<String> get()
     {
-        return ResponseEntity.ok("Hello World");
+        return ResponseEntity.ok("Hello Sample");
     }
 }
